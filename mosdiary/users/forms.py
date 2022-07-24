73254ad402529278bfe206780edd1f8b6e8f1be3
Mohamed Elgazzar <momoelgazzar@gmail.com>
2022-07-24 20:02:45 +0200

    delete unnecessary files

--- mosdiary/users/forms.py
+++ mosdiary/users/forms.py
@@ -1,11 +1,9 @@
-# Form Based Imports
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo
 from wtforms import ValidationError
 from flask_wtf.file import FileField, FileAllowed
 
-# User Based Imports
 from flask_login import current_user
 from mosdiary.models import User
 
