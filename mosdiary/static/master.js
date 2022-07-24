864eceead276ec091fce09f0a43acc156c3e7d9d
moelgazzar <momoelgazzar@gmail.com>
2022-07-24 16:56:09 +0200

    first commit

---
+++ mosdiary/static/master.js
@@ -0,0 +1,50 @@
+/* ## */
+/* H1 */
+/* ## */
+
+  var words = ["📖 Welcome to Mø's Diary!", '🤡 Mo is a beast with many personalities.', '🤬 Each personality has a say.', '📖 This Diary provides an account to each personality.', "🧠 This is an attempt to improve Mo's mental health!"],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 50,
+    speed = 50;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
