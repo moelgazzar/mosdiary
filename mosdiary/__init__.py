5b7ff0b44f533aed13c92573a0108aed76129332
Mohamed Elgazzar <momoelgazzar@gmail.com>
2022-07-30 01:03:47 +0200

    CSRF Protection

--- mosdiary/__init__.py
+++ mosdiary/__init__.py
@@ -3,18 +3,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+csrf = CSRFProtect(app)
 
 #################################
 ### DATABASE SETUPS
 ###############################
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, str(os.environ.get("DB_NAME", "")))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
